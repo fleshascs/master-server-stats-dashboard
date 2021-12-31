aa56968de6efa49ab819867ca36dea63a626d4cb
fleshascs <35168446+fleshascs@users.noreply.github.com>
2021-12-31 17:44:22 +0200

    cleanup

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -31,8 +31,8 @@ export default function NavBar() {
               </div>
               <div className='flex-1 flex items-center justify-center sm:items-stretch sm:justify-start'>
                 <div className='flex-shrink-0 flex items-center'>
-                  <img className='block lg:hidden h-8 w-auto' src='192x192.png' alt='Workflow' />
-                  <img className='hidden lg:block h-8 w-auto' src='logo.png' alt='Workflow' />
+                  <img className='block lg:hidden h-10 w-auto' src='192x192.png' alt='Workflow' />
+                  <img className='hidden lg:block h-10 w-auto' src='logo.png' alt='Workflow' />
                 </div>
                 <div className='hidden sm:block sm:ml-6'>
                   <div className='flex space-x-4'>
