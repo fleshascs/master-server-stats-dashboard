a350123a82fbce915e4f1affe06a4616374d1378
fleshascs <35168446+fleshascs@users.noreply.github.com>
2022-02-04 16:37:11 +0200

    calc inc for 0 values

--- components/utils.ts
+++ components/utils.ts
@@ -1,4 +1,6 @@
 export function calculateIncrease(startingValue: number, finalValue: number) {
+  startingValue += 1;
+  finalValue += 1;
   const value = ((finalValue - startingValue) / startingValue) * 100;
   return Math.round(value * 10) / 10;
 }
