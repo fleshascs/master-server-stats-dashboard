9c84e74e852e4a61731e24a3f8f1ab218be29d2a
fleshascs <35168446+fleshascs@users.noreply.github.com>
2022-07-07 18:08:53 +0300

    added ms server details

--- components/MasterServersTabs/QueryMasterServerPlayers.tsx
+++ components/MasterServersTabs/QueryMasterServerPlayers.tsx
@@ -1,15 +1,50 @@
-import { FC } from 'react';
-
+import { FC, useEffect, useState } from 'react';
 import { useQuery } from 'react-query';
 import Card from '../Card';
+
+async function* getServerDetails(ips: string[]) {
+  for (const ip of ips) {
+    const details = await fetch('https://cs-boost.lt/api/server_info_by_ip.php?ip=' + ip).then(
+      (res) => res.json()
+    );
+    yield details.server;
+  }
+}
+
+const collectServerDetails = (ips, update) => {
+  let stop = false;
+  (async () => {
+    for await (const details of getServerDetails(ips)) {
+      update((d) => ({ ...d, [details.address]: details }));
+      if (stop) break;
+    }
+  })();
+
+  return () => {
+    stop = true;
+  };
+};
 
 export const QueryMasterServerPlayers: FC<{ server: string; isAlive: boolean }> = ({
   server,
   isAlive
 }) => {
-  const { isLoading, error, data } = useQuery(['ms-server-list-players', server], () =>
-    fetch(process.env.apiUrl + '/api/vmsq/' + server).then((res) => res.json())
+  const { isLoading, error, data } = useQuery<string[], Error>(
+    ['ms-server-list-players', server],
+    () => fetch(process.env.apiUrl + '/api/vmsq/' + server).then((res) => res.json())
   );
+
+  const [serversInfo, setServersInfo] = useState({});
+
+  useEffect(() => {
+    let stopCollecting;
+    if (data?.length) {
+      stopCollecting = collectServerDetails(data, setServersInfo);
+    }
+    return () => {
+      // stopCollecting && stopCollecting();
+    };
+  }, [data]);
 
   if (isLoading) return <Card title={server}>Loading...</Card>;
   if (error) return <Card title={server}>An error has occurred</Card>;
@@ -18,10 +53,17 @@ export const QueryMasterServerPlayers: FC<{ server: string; isAlive: boolean }> 
     <Card title={`${server} is ${isAlive || data ? 'online' : 'offline'}`}>
       <div>
         <h4 className='p-3 '>Server list:</h4>
-        <ul className='list-decimal list-inside overflow-y-auto p-3'>
-          {data.map((ip) => (
-            <li className='p-1' key={ip}>
-              {ip}
+        <ul className='list-none list-inside overflow-y-auto p-3'>
+          {data.map((ip, i) => (
+            <li className='p-1 text-xs' key={ip}>
+              <div className='flex space-x-3'>
+                <div>{i + 1}. </div>
+                <div>{ip}</div>
+                <div className='flex-1 truncate'>{serversInfo[ip]?.hostname}</div>
+                <div>
+                  {serversInfo[ip]?.players}/{serversInfo[ip]?.maxplayers}
+                </div>
+              </div>
             </li>
           ))}
         </ul>
